8273ff8432357c77b2fc7092cc5cc2c9aacb33bc
Thomas Depian <thomas.depian99@gmail.com>
2018-06-04 20:16:51 +0200

    bugfix bei save-failed-login.php

--- Webseite_VEVETA/Webserver/save-failed-login.php
+++ Webseite_VEVETA/Webserver/save-failed-login.php
@@ -1,14 +1,13 @@
 <?php
-  include 'connect-database.php';
 
   function writeToDatabase($login='', $pass='', $wordpress=0){
+    include 'connect-database.php';
+
     $ip = getenv('REMOTE_ADDR');
     $datum = date('Y-m-d H-i-s');
 
     $statement =  $mysqli->prepare("INSERT INTO failedlogins (ip, datum, benutzername, passwort, wordpress) VALUES ('".$ip."', '".$datum."', '?', '?', '?');");
     $statement->bind_param("ssi", $login, $pass, $wordpress);
     $statement->execute();
-
-
   }
 ?>
