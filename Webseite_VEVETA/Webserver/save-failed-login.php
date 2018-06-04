db304bd2d0505b02213234ab8cbfe2c70adf48d6
Thomas Depian <thomas.depian99@gmail.com>
2018-06-04 20:21:54 +0200

    warnung bei save-failed-login.php gefixt

--- Webseite_VEVETA/Webserver/save-failed-login.php
+++ Webseite_VEVETA/Webserver/save-failed-login.php
@@ -6,7 +6,7 @@
     $ip = getenv('REMOTE_ADDR');
     $datum = date('Y-m-d H-i-s');
 
-    $statement =  $mysqli->prepare("INSERT INTO failedlogins (ip, datum, benutzername, passwort, wordpress) VALUES ('".$ip."', '".$datum."', '?', '?', '?');");
+    $statement =  $mysqli->prepare("INSERT INTO failedlogins (ip, datum, benutzername, passwort, wordpress) VALUES ('".$ip."', '".$datum."', ?, ?, ?);");
     $statement->bind_param("ssi", $login, $pass, $wordpress);
     $statement->execute();
   }
