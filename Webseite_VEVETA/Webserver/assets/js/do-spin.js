f682ba7e9bf46eb4d920e2dc82edeff0712dfad9
Thomas Depian <thomas.depian99@gmail.com>
2018-06-09 12:25:24 +0200

    reply wird geupdated

--- Webseite_VEVETA/Webserver/assets/js/do-spin.js
+++ Webseite_VEVETA/Webserver/assets/js/do-spin.js
@@ -1,4 +1,5 @@
 function addspinn() {
+  window.scrollTo(0, 0);
   $("#outer-maxi-spinner").css("z-index",99);
   $("#maxi-spinner").css("display","");
   location.reload();
---
+++ Webseite_VEVETA/Webserver/reply-test.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <!-- METATAGS -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="My Maturaprojekt - Tarpit">
+  <meta name="keywords" content="Tarpit, tarpit, Html, php, Matura, Maturaprojekt">
+  <meta name="author" content="Thomas Brixen">
+  <!-- TITLE -->
+  <title>VEVETA - Maturaprojekt</title>
+
+  <!-- favicon -->
+  <link rel="apple-touch-icon" sizes="180x180" href="assets/favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="assets/favicon/favicon-16x16.png">
+  <link rel="manifest" href="assets/favicon/site.webmanifest">
+  <link rel="mask-icon" href="assets/favicon/safari-pinned-tab.svg" color="#aaaaaa">
+  <link rel="shortcut icon" href="assets/favicon/favicon.ico">
+  <meta name="msapplication-TileColor" content="#e4e7ec">
+  <meta name="msapplication-config" content="assets/favicon/browserconfig.xml">
+  <meta name="theme-color" content="#e4e7ec">
+
+  <!-- Stylesheets, etc -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat+Subrayada">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Orbitron">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+  <link rel="stylesheet" href="assets/css/Customizable-Background--Overlay.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.1.1/aos.css">
+  <link rel="stylesheet" href="assets/css/styles.css">
+  <link rel="stylesheet" href="assets/css/Timeline.css">
+</head>
+
+<body>
+    <div class="container container-content">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="text-center subtitle-no-tranform">TEST</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-4">
+                <h3>Spalte1</h3>
+                <p class="text-content generated-text"><br>Diese Webseite ist Bestandteil meines Maturaprojektes, welches ich für die Matura 2017/18 entwickelt habe.&nbsp;<br>Mein Maturaprojekt beschäftigt sich mit der Beobachtung und Analyse von Webcrawlern und die Erkennung/Beweis von bekannten
+                    Angriffsschemas.&nbsp;<br>Um diese Angriffsschemas zu erforschen habe ich eine Teergrube entwickelt, welche Teil dieser Webseite ist. Im Demo-Bereich können Websurfer auf die Tarpit zugreifen.&nbsp;<br>Zu den bekannten Angriffsmustern
+                    gehören:<br><br></p>
+            </div>
+            <div class="col-4">
+                <h3>Spalte2</h3>
+                <p class="text-content"><br>Diese Webseite ist Bestandteil meines Maturaprojektes, welches ich für die Matura 2017/18 entwickelt habe.&nbsp;<br>Mein Maturaprojekt beschäftigt sich mit der Beobachtung und Analyse von Webcrawlern und die Erkennung/Beweis von bekannten
+                    Angriffsschemas.&nbsp;<br>Um diese Angriffsschemas zu erforschen habe ich eine Teergrube entwickelt, welche Teil dieser Webseite ist. Im Demo-Bereich können Websurfer auf die Tarpit zugreifen.&nbsp;<br>Zu den bekannten Angriffsmustern
+                    gehören:<br><br></p>
+            </div>
+            <div class="col-4">
+                <h3>Spalte3</h3>
+                <p class="text-content"><br>Diese Webseite ist Bestandteil meines Maturaprojektes, welches ich für die Matura 2017/18 entwickelt habe.&nbsp;<br>Mein Maturaprojekt beschäftigt sich mit der Beobachtung und Analyse von Webcrawlern und die Erkennung/Beweis von bekannten
+                    Angriffsschemas.&nbsp;<br>Um diese Angriffsschemas zu erforschen habe ich eine Teergrube entwickelt, welche Teil dieser Webseite ist. Im Demo-Bereich können Websurfer auf die Tarpit zugreifen.&nbsp;<br>Zu den bekannten Angriffsmustern
+                    gehören:<br><br></p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col"><a class="btn btn-primary btn-block back-button" role="button" href="index.html">Zur Startseite</a></div>
+        </div>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/-Animated-numbers-section-BS4-.js"></script>
+    <script src="assets/js/bs-animation.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.1.1/aos.js"></script>
+</body>
+
+</html>
