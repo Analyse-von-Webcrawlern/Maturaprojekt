72dec1b95347788347f82e7ce48120f4c712f688
Thomas Depian <thomas.depian99@gmail.com>
2018-06-09 20:53:34 +0200

    i bin sooo dumm

--- Webseite_VEVETA/Webserver/failedlogins.php
+++ Webseite_VEVETA/Webserver/failedlogins.php
@@ -73,7 +73,7 @@
       <?php
         include 'connect-database.php';
 
-        $statement = "SELECT *, DATE_FORMAT(datum, '%a, $d. %b %Y $T') AS formatiert FROM failedlogins";
+        $statement = "SELECT *, DATE_FORMAT(datum, '%a, %d. %b %Y $T') AS formatiert FROM failedlogins";
         $result = $mysqli->query($statement);
         while ($row = $result->fetch_assoc()) {
           echo "<tr>";
@@ -130,35 +130,9 @@
             }
 
           },
-
-          language: {
-        processing:     "Traitement en cours...",
-        search:         "Rechercher&nbsp;:",
-        lengthMenu:    "Afficher _MENU_ &eacute;l&eacute;ments",
-        info:           "Affichage de l'&eacute;lement _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
-        infoEmpty:      "Affichage de l'&eacute;lement 0 &agrave; 0 sur 0 &eacute;l&eacute;ments",
-        infoFiltered:   "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
-        infoPostFix:    "",
-        loadingRecords: "Chargement en cours...",
-        zeroRecords:    "Aucun &eacute;l&eacute;ment &agrave; afficher",
-        emptyTable:     "Aucune donnée disponible dans le tableau",
-        paginate: {
-            first:      "Premier",
-            previous:   "Pr&eacute;c&eacute;dent",
-            next:       "Suivant",
-            last:       "Dernier"
-        },
-        aria: {
-            sortAscending:  ": activer pour trier la colonne par ordre croissant",
-            sortDescending: ": activer pour trier la colonne par ordre décroissant"
-        }
-    }
-
-
           "fixedHeader": true,
           "responsive": true
         } );
-        //$('#failedlogins').css("display","block");
   		});
 	   </script>
 </body>
