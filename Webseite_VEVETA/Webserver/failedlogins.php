4257686ae9bc890f42b594e100cb45a0edc8611c
Thomas Depian <thomas.depian99@gmail.com>
2018-06-09 12:10:43 +0200

    Favicon auf allen Webseiten geupdated

--- Webseite_VEVETA/Webserver/failedlogins.php
+++ Webseite_VEVETA/Webserver/failedlogins.php
@@ -11,6 +11,17 @@
   <!-- TITLE -->
   <title>VEVETA - Maturaprojekt</title>
 
+  <!-- favicon -->
+  <link rel="apple-touch-icon" sizes="180x180" href="assets/favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="assets/favicon/favicon-16x16.png">
+  <link rel="manifest" href="assets/favicon/site.webmanifest">
+  <link rel="mask-icon" href="assets/favicon/safari-pinned-tab.svg" color="#aaaaaa">
+  <link rel="shortcut icon" href="assets/favicon/favicon.ico">
+  <meta name="msapplication-TileColor" content="#e4e7ec">
+  <meta name="msapplication-config" content="assets/favicon/browserconfig.xml">
+  <meta name="theme-color" content="#e4e7ec">
+
   <!-- Stylesheets, etc -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -25,8 +36,7 @@
   <link rel="stylesheet" href="assets/css/Timeline.css">
 
 
-  <!-- <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.css"/> -->
-  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap4.min.css"/>
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.css"/>
 
   <!--Google Analytics-->
   <script>
